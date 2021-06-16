5fc04c0d8c4c2bbc5c7bb10431723f2e9ddf5cfc
Ovidiu Turcu <ovi_tsb@yahoo.com>
2021-06-16 19:09:41 -0400

    Fixing the js on IE

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -48,21 +48,19 @@ jQuery(document).ready(function($) {
 
 //////  GO TO Page /////////////////////
 
-
-$(document).ready(function () {
-        $('#selectAll').click(function (event) {
-            if (this.checked) {
-                $(':checkbox').each(function () {
-                    this.checked = true;
-                });
-            } else {
-                $(':checkbox').each(function () {
-                    this.checked = false;
-                });
-            }
-        });
-
-    });
+// $(document).ready(function () {
+  $('#selectAll').click(function (event) {
+    if (this.checked) {
+      $(':checkbox').each(function () {
+        this.checked = true;
+      });
+    } else {
+      $(':checkbox').each(function () {
+        this.checked = false;
+      });
+    }
+  });
+// });
 
 
 
