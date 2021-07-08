bf37502dd53886f4e6840bb394200c0cf6f720df
Ovidiu Turcu <ovi_tsb@yahoo.com>
2021-07-08 14:39:31 -0400

    Comment the csv file on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,26 +29,17 @@ puts "Mississauga plant created."
 
 
 #####  CSV London
-require 'csv'
 
-CSV.foreach("lib/seeds/CSVLondonParts.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
-  Product.create(row.to_hash)
-end
 
-# puts "DB Updated."
+# require 'csv'
 
-# csv_text = File.read(Rails.root.join('lib', 'seeds', 'CSVLondonParts.csv'))
-# csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
-# csv.each do |row|
-# 	p = Product.new
-# 	p.item_no = row['item_no']
-# 	p.description = row['description']
-# 	p.qty = row['qty']
-# 	p.location = row['location']
-# 	p.plant_id = row['plant_id']
+# CSV.foreach("lib/seeds/CSVLondonParts.csv", { encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all}) do |row|
+#   Product.create(row.to_hash)
 # end
 
-puts "There are now #{Product.count} rows in the product table"
+
+
+# puts "There are now #{Product.count} rows in the product table"
 
 
 
