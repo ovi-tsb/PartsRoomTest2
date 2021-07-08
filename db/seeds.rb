f651411cd0402c585655612df092fc465639a50b
Ovidiu Turcu <ovi_tsb@yahoo.com>
2021-07-08 14:31:25 -0400

    Added seed file on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,20 +9,20 @@
 
 
 
-# @user = SuperUser.create!(email: "super@yahoo.com", 
-#                     password: "123456", 
-#                     password_confirmation: "123456", 
-#                     first_name: "Tom", 
-#                     last_name: "Hood"
-#                   )
-# puts "Super user created."
+@user = SuperUser.create!(email: "super@yahoo.com", 
+                    password: "123456", 
+                    password_confirmation: "123456", 
+                    first_name: "Tom", 
+                    last_name: "Hood"
+                  )
+puts "Super user created."
 
 
-# @plant = Plant.create(name: "London")
-# puts "London plant created."
+@plant = Plant.create(name: "London")
+puts "London plant created."
 
-# @plant1 = Plant.create(name: "Mississauga")
-# puts "Mississauga plant created."
+@plant1 = Plant.create(name: "Mississauga")
+puts "Mississauga plant created."
 
 
 
